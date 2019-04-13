4727570bdd56bbced48c7e8e42639898245b91ae
Zack Bunn <zrb6578@gmail.com>
2019-04-13 16:18:44 -0500

    updated format

--- src/app.js
+++ src/app.js
@@ -1,4 +1,3 @@
-require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
@@ -7,27 +6,25 @@ const { NODE_ENV } = require('./config')
 
 const app = express()
 
-const morganOption = (NODE_ENV === 'production')
-  ? 'tiny'
-  : 'common';
-
-app.use(morgan(morganOption))
+app.use(morgan((NODE_ENV === 'production') ? 'tiny' : 'common', {
+  skip: () => NODE_ENV === 'test',
+}))
 app.use(cors())
 app.use(helmet())
 
 app.get('/', (req, res) => {
-    res.send('Hello, world!')
+  res.send('Hello, world!')
 })
 
 app.use(function errorHandler(error, req, res, next) {
-   let response
-   if (NODE_ENV === 'production') {
-     response = { error: { message: 'server error' } }
-   } else {
-     console.error(error)
-     response = { message: error.message, error }
-   }
-   res.status(500).json(response)
+  let response
+  if (NODE_ENV === 'production') {
+    response = { error: 'server error' }
+  } else {
+    console.error(error)
+    response = { error: error.message, details: error }
+  }
+  res.status(500).json(response)
 })
-    
+
 module.exports = app
